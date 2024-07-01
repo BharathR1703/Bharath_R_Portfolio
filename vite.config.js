80edf19f92ce02088f647b06689595d4cec53423
BharathR1703 <92417111+BharathR1703@users.noreply.github.com>
2024-07-01 14:35:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,  // Automatically open the app in the browser
+    port: 5500   // Ensure the port matches your setup
+  }
+});
