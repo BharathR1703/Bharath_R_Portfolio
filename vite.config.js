3a3c5285689d109af501641901fc34e557951982
BharathR1703 <92417111+BharathR1703@users.noreply.github.com>
2024-07-01 15:30:12 +0530

    cg

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base:"https://bharathr1703.github.io/Bharath_R_Portfolio/"
+});
